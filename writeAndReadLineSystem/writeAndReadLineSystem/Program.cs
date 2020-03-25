33914afc5794ce14379d55760a5a6f24052f7fbd
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-03-25 13:45:38 +0000

    Add files via upload

---
+++ writeAndReadLineSystem/writeAndReadLineSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace writeAndReadLineSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your name");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+            Console.WriteLine("What is your age?");
+
+            var age = Console.ReadLine();
+
+            Console.WriteLine($"Your age is {age}");
+
+            Console.WriteLine("What is your gender? Male or Female");
+
+            var gender = Console.ReadLine();
+
+            Console.WriteLine($"Your gender is {gender}");
+             
+            Console.WriteLine("Thank you for completing this short questionnaire");
+
+            Console.ReadKey();
+
+        }
+    }
+} 
