a1fd4afa779c4d9a3ebc5add7e951a56e310e73d
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-03-28 12:26:16 +0000

    Add files via upload

---
+++ dataTypeConversion/dataTypeConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace dataTypeConversion
+{
+    class Program
+    {
+        public struct MoneyConversion
+        {
+            public float gbp;
+            public MoneyConversion(float r)
+            {
+                this.gbp = r;
+
+            }
+            public static explicit operator MoneyConversion(int usd)
+            {
+                float convertRate = 1.25f * usd;
+         
+                MoneyConversion temp = new MoneyConversion(convertRate);
+
+                return temp;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an amount from GBP which can be converted to USD");
+
+            int mm = Convert.ToInt32(Console.ReadLine());
+
+            MoneyConversion im = (MoneyConversion)mm;
+
+            Console.WriteLine($"The conversion of {mm} is {im.gbp} in USD");
+
+            Console.ReadKey();
+
+        }
+    }
+}
+ 
