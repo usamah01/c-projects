bca18df67dfd64a3971bac81ee187d762bc6a399
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-07-19 18:03:29 +0100

    Add files via upload

---
+++ HelloWorld/HelloWorld.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld.UI
+{
+    class program
+    {
+        static void Main(string[] args)
+        { 
+        Console.WriteLine("Hello World");
+            Console.ReadLine();
+}
+
+}
+}
